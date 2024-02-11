83fc668826f57030d0eb653ac0de524ff07f2bd5
Haadi Rakhangi <127193364+haadirakhangi@users.noreply.github.com>
2024-02-11 17:53:38 +0530

    nothing

--- chainlit.py
+++ chainlit.py
@@ -1,28 +1,36 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema import StrOutputParser
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from langchain.schema.runnable import Runnable
+from langchain.agents.agent_types import AgentType
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+from langchain_openai import ChatOpenAI, OpenAI
 from langchain.schema.runnable.config import RunnableConfig
 import openai
 import chainlit as cl
+import pandas as pd
 import os
 from dotenv import load_dotenv,find_dotenv
 load_dotenv(find_dotenv())
 openai.api_key = os.environ['OPENAI_API_KEY']
+
+df = pd.read_csv("d2k_data.csv")
+
 @cl.on_chat_start
 async def on_chat_start():
-    model = ChatOpenAI(streaming=True)
+    model = ChatOpenAI(model='gpt-3.5-turbo-1106',streaming=True)
     prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
-                "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
+                "You're a helpful csv assistant",
             ),
-            ("human", "{question}"),
+            ("human", "{input}"),
         ]
     )
     runnable = prompt | model | StrOutputParser()
-    cl.user_session.set("runnable", runnable)
+    agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=True, agent_type = AgentType.OPENAI_FUNCTIONS)
+    cl.user_session.set("runnable", agent)
 
 
 @cl.on_message
@@ -31,8 +39,9 @@ async def on_message(message: cl.Message):
 
     msg = cl.Message(content="")
 
+    print(runnable.invoke({"input": message.content}))
     async for chunk in runnable.astream(
-        {"question": message.content},
+        {"input": message.content},
         config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
     ):
         await msg.stream_token(chunk)
